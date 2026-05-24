2a298cc77732a4e0e0f4050f44d486850040c024
TerraBitten <winterfrost147aj@gmail.com>
2026-05-24 11:22:35 +0930

    Update s1.js

--- static/assets/js/s1.js
+++ static/assets/js/s1.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (selectedValue === "popups") {
       localStorage.setItem("ads", "popups");
     } else if (selectedValue === "off") {
-      localStorage.setItem("ads", "off");
+      localStorage.setItem("ads", "on");
     }
   }
 
@@ -345,19 +345,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function getRandomURL() {
   const randomURLS = [
-    "https://kahoot.it",
-    "https://classroom.google.com",
-    "https://drive.google.com",
-    "https://google.com",
-    "https://docs.google.com",
-    "https://slides.google.com",
-    "https://www.nasa.gov",
-    "https://blooket.com",
-    "https://clever.com",
-    "https://edpuzzle.com",
-    "https://khanacademy.org",
-    "https://wikipedia.org",
-    "https://dictionary.com",
+
   ];
   return randomURLS[randRange(0, randomURLS.length)];
 }
